0920c110109ebd40788119e63794466be40a03ca
raymondtju <raymond.rtju@gmail.com>
2024-02-20 11:28:19 +0700

    D03: quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -23,42 +23,53 @@
 // I AM NOT DONE
 
 pub enum Command {
-    Uppercase,
-    Trim,
-    Append(usize),
+  Uppercase,
+  Trim,
+  Append(usize),
 }
 
 mod my_module {
-    use super::Command;
+  use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
-        for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+  // TODO: Complete the function signature!
+  pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+    // TODO: Complete the output declaration!
+    let mut output: Vec<String> = vec![];
+    for (string, command) in input.iter() {
+      match command {
+        Command::Uppercase => {
+          // let mut outputs = [string.to_uppercase()].to_vec();
+          // output.append(&mut outputs);
+          output.push(string.to_uppercase())
         }
-        output
+        Command::Trim => output.append(&mut ([String::from(string.trim())].to_vec())),
+        Command::Append(x) => {
+          let appended_string = format!("{}{}", string, "bar".repeat(*x));
+          output.push(appended_string);
+        }
+      }
     }
+    output
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
-    use super::Command;
+  // TODO: What do we need to import to have `transformer` in scope?
+  use super::my_module::transformer;
+  use super::Command;
 
-    #[test]
-    fn it_works() {
-        let output = transformer(vec![
-            ("hello".into(), Command::Uppercase),
-            (" all roads lead to rome! ".into(), Command::Trim),
-            ("foo".into(), Command::Append(1)),
-            ("bar".into(), Command::Append(5)),
-        ]);
-        assert_eq!(output[0], "HELLO");
-        assert_eq!(output[1], "all roads lead to rome!");
-        assert_eq!(output[2], "foobar");
-        assert_eq!(output[3], "barbarbarbarbarbar");
-    }
+  #[test]
+  fn it_works() {
+    let output = transformer(vec![
+      ("hello".into(), Command::Uppercase),
+      (" all roads lead to rome! ".into(), Command::Trim),
+      ("foo".into(), Command::Append(1)),
+      ("bar".into(), Command::Append(5)),
+    ]);
+    assert_eq!(output[0], "HELLO");
+    assert_eq!(output[1], "all roads lead to rome!");
+    assert_eq!(output[2], "foobar");
+    assert_eq!(output[3], "barbarbarbarbarbar");
+  }
 }
